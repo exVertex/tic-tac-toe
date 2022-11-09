e5a7d759d072983d977ffd6746c17a0a114b2c80
PETRA TANKOVIĆ <pt6346@student.uni-lj.si>
2022-11-09 07:42:11 +0100

    Add initial menu set-up

---
+++ src/script.ts
@@ -0,0 +1,7 @@
+// Menu 
+const menuToggle = document.getElementById("toggle-menu");
+const mainContainer = document.getElementById("main-container");
+
+menuToggle?.addEventListener('click', () => {
+    mainContainer?.classList.toggle("active");
+});
