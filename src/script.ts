a37f833541d09bd72de49b04cacf150bfc9f4ded
Petra <petra.tankovic@gmail.com>
2022-11-11 11:41:43 +0100

    Add notImplemented() function

--- src/script.ts
+++ src/script.ts
@@ -5,3 +5,7 @@ const mainContainer = document.getElementById("main-container");
 menuToggle?.addEventListener('click', () => {
     mainContainer?.classList.toggle("in-active");
 });
+
+function notImplemented(): void {
+    alert("This feature is not yet implemented. Plaese come back later.");
+}
